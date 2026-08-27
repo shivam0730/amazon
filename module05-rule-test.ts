c5ec029d927daeb9190056579694549c937164f1
Shivam Rajput <shivamrajput1351@gmail.com>
2026-08-27 18:14:22 +0530

    Add initial module05-rule-test.ts file
    Initial implementation of module05-rule-test.ts with placeholders for TODO and FIXME.

---
+++ module05-rule-test.ts
@@ -0,0 +1,15 @@
+// TODO: finish this
+// FIXME: remove this
+
+const apiKey = "sk-test-secret-value-123456789";
+
+console.log("debug");
+
+try {
+  doSomething();
+} catch (error) {
+}
+
+const result = eval(userInput);
+
+const html = document.body.innerHTML = userInput;
